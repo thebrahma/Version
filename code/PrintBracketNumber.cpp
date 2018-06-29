87458bcf91b4deaf2e1fac36a126b2674694c084
thebrahma <utpalbrahma1995@gmail.com>
2018-06-29 07:18:37 +0530

    edited

--- code/PrintBracketNumber.cpp
+++ code/PrintBracketNumber.cpp
@@ -1,87 +1,33 @@
-#include <bits/stdc++.h>
-#include<stack>
+#include<bits/stdc++.h>
 using namespace std;
-
-int Prec(char ch)
-{
-    switch (ch)
-    {
-    case '+':
-    case '-':
-        return 1;
- 
-    case '*':
-    case '/':
-        return 2;
- 
-    case '^':
-        return 3;
-    }
-    return -1;
-}
-
-
-void infixtopostfix(char ch[],char exp[],stack<int> *stk)
-{
-	int i=0,k=-1;
-    while(ch[i]!='\0')
-    {
-    		if((ch[i]>='a' && ch[i]<='z') || (ch[i]>='A' && ch[i]<='Z')){
-    			exp[++k] = ch[i];
-            
-    		}
-    		else if(ch[i]=='('){
-    			stk->push(ch[i]);
-    		
-    		}
-    		else if(ch[i]==')')
-    		{
-    				while( !stk->empty() && stk->top()!='(')
-    				{
-    					exp[++k] = stk->top();
-    					stk->pop();
-    			
-    				}
-    				if((!stk->empty()) && (stk->top()!='(')){
-    					printf("Invaid Syntax\n");
-    					break; 
-    				
-    				}
-    				else{
-    					stk->pop();
-    	
-    				}
-    		}
-    		else
-    		{
-         			while(!stk->empty() && Prec(ch[i]) <= Prec(stk->top()))
-         			{
-         				exp[++k] = stk->top();
- 
-         				stk->pop();
-         			}
-         			stk->push(ch[i]);
-         			cout<<stk->top()<<"\n";
-    		}
-    		i++;
-    }  
-    while (!stk->empty())
-    {
-    	exp[++k]=stk->top();
-    	stk->pop();
-    
-    }
- 
-    exp[++k] = '\0';
-}
-
-
 int main()
 {
-	char ch[100],exp[100];
-	cin>>ch;
-	stack<int> stk;
-	infixtopostfix(ch,exp,&stk);
-	printf("\n%s\n",exp );
-    return 0;
+	int T;
+	stack <int> stk;
+	scanf("%d",&T);
+	char ch[200];
+	while(T--)
+	{
+		scanf("%s",ch);
+		int i=0,k=0;
+		while(ch[i]!='\0')
+		{
+				if(ch[i]=='(')
+				{
+					k++;
+					stk.push(k);
+					printf("%d",k );
+				}
+				if(ch[i]==')')
+				{
+					int temp = stk.top();
+					printf("%d",temp );
+					stk.pop();
+				}
+				i++;
+		}
+		printf("\n");
+	}
+
+	return 0;
 }
